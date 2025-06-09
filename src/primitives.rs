847cf3563d888cdf8658f230249092503d041e1b
Yonatan Reicher <yony252525@gmail.com>
2025-06-09 10:50:10 +0300

    Add more Parser::char like functions

--- src/primitives.rs
+++ src/primitives.rs
@@ -31,6 +31,53 @@ impl<'a, E: 'a> Parser<'a, char, E, EofFailure> {
     }
 }
 
+impl<'a, F: Clone + Default + 'a, E: 'a> Parser<'a, char, E, F> {
+    pub fn char_eq(ch: char) -> Parser<'a, char, E, F> {
+        Parser::char()
+            .map_fail(|_| F::default())
+            .filter(move |&c| c == ch, F::default())
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct NotWhitespace;
+
+impl<'a, E: 'a> Parser<'a, char, E, NotWhitespace> {
+    pub fn whitespace() -> Parser<'a, char, E, NotWhitespace> {
+        Parser::char()
+            .map_fail(|_| NotWhitespace)
+            .filter(|c| c.is_whitespace(), NotWhitespace)
+    }
+}
+
+impl<'a, F: 'a, E: 'a> Parser<'a, (), E, F> {
+    pub fn skip_whitespace() -> Parser<'a, (), E, F> {
+        Parser::whitespace().repeat_0().map(|_| ())
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct NotALetter;
+
+impl<'a, E: 'a> Parser<'a, char, E, NotALetter> {
+    pub fn letter() -> Parser<'a, char, E, NotALetter> {
+        Parser::char()
+            .map_fail(|_| NotALetter)
+            .filter(|c| c.is_ascii_alphabetic(), NotALetter)
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct NotADigit;
+
+impl<'a, E: 'a> Parser<'a, char, E, NotADigit> {
+    pub fn digit() -> Parser<'a, char, E, NotADigit> {
+        Parser::char()
+            .map_fail(|_| NotADigit)
+            .filter(|c| c.is_ascii_digit(), NotADigit)
+    }
+}
+
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct NotFound;
 
